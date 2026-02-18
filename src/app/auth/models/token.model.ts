6cde1a086cbf161667ebb749fb14daa3d1a5777b
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2026-02-17 22:05:42 -0300

    core: erstructura inicial para Auth

--- src/app/auth/models/token.model.ts
+++ src/app/auth/models/token.model.ts
@@ -1,8 +1,8 @@
 export interface JwtPayload {
   sub: string;
   ownerId: string;
-  role?: Role;
-  purpose?: TokenPurpose;
+  role: Role;
+  purpose: TokenPurpose;
 }
 export type TokenPurpose = 'ACCESS' | 'PASSWORD_SETUP' | 'REFRESH';
 
